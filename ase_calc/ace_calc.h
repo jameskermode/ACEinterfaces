7f040a048c191be4e4cfba8c11a60f0c43bff86e
Christoph Ortner <christophortner0@gmail.com>
2021-11-07 09:30:26 -0800

    remove more stuff

--- ase_calc/ace_calc.h
+++
@@ -1,6 +0,0 @@
-/* Function prototypes for ace_calc.c */
-
-void init_calc(char IP_name);
-double energy(double* cell, short* bc, double* positionss, int* Z);
-double forces(double* cell, short* bc, double* positionss, int* Z);
-double stress(double* cell, short* bc, double* positionss, int* Z);
